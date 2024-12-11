3e5d10949168fec147a707ba4e2ebe61b3d69be6
leonidassacu <162514466+leonidassacu@users.noreply.github.com>
2024-12-11 11:20:46 +0000

    subir todos os arquivos do meu site

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function enviar() {
+
+    var sms= document.getElementById('isms')
+    var res = document.getElementById('res')
+    var arquivo = document.getElementById('arquivo')
+    var s = (isms.value)
+    
+   
+    
+    res.innerHTML = `${s}`
+    
+}
